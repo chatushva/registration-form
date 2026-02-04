893031fd6c7f49dbfd61e4791b540f600e848b83
immidisetti chatushva <immidisettichatushva@gmail.com>
2026-02-04 23:50:49 +0530

    Update DetailsPage.jsx

--- reactform/src/DetailsPage.jsx
+++ reactform/src/DetailsPage.jsx
@@ -1,7 +1,6 @@
 
 import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-
 function DetailsPage() {
   const navigate = useNavigate();
   const location = useLocation();
